298dfa69b42416500605ea6cd6216b92350e3393
Karan Anand <thekarananand@gmail.com>
2025-11-14 10:57:47 +0530

    Change server port from 3000 to 8000

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors    = require("cors");
 const app     = express();
-const port    = 3000;
+const port    = 8000;
 
 // Read appName env variable
 const appName = process.env.APP_NAME || "< No APP_NAME found >";
